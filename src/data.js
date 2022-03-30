5f922e091f66701c808edfe2be28eee86ceb9f8d
Jethro Dean <jethroldean@gmail.com>
2022-03-30 19:16:35 +1100

    Add initial data, images, & components

---
+++ src/data.js
@@ -0,0 +1,83 @@
+export default [
+    {
+        title: "Auckland", 
+        location: "New Zealand", 
+        googleMapsUrl: "https://goo.gl/maps/YMpTpNUkksLP8a91A",
+        startDate: "Nov 2018", 
+        endDate: "Nov 2018", 
+        description: "Auckland, based around 2 large harbours, is a major city in the north of New Zealand’s North Island. In the centre, the iconic Sky Tower has views of Viaduct Harbour, which is full of superyachts and lined with bars and cafes.",
+        imageUrl: "./images/auckland.jpg"
+    },
+    {
+        title: "Chiang Mai",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/oMMmLvHXK5JGxxuG7",
+        startDate: "Nov 2015",
+        endDate: "Aug 2016",
+        description: "Chiang Mai, located in a mountainous region, is the largest city in northern Thailand and the second largest city in Thailand. Its Old City area still retains vestiges of walls and moats from its history as a cultural and religious center.",
+        imageUrl: "./images/chiang-mai.webp",
+    },
+    {
+        title: "Chanthaburi",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/t7Gc9fYUNjnHNGM87",
+        startDate: "June 2015",
+        endDate: "June 2015",
+        description: "Chanthaburi is a province in southeast Thailand, fringing the Gulf of Thailand and known for gem dealing and tropical fruit.",
+        imageUrl: "./images/chanthaburi.jpg",
+    },
+    {
+        title: "Pattaya",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/AemeKLnwABv8bhqH7",
+        startDate: "June 2015",
+        endDate: "June 2015",
+        description: "Pattaya is a city on Thailand’s eastern Gulf coast known for its beaches. The Pattaya Bay area is one of Asia's largest beach resorts and the second most visited city in Thailand, after Bangkok.",
+        imageUrl: "./images/pattaya.jpg"
+    },
+    {
+        title: "Bangkok",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/zWwhd2weVUL3vDze9",
+        startDate: "June 2015",
+        endDate: "July 2015",
+        description: "Bangkok is the capital and most populous city of Thailand. It is a large city known for ornate shrines and vibrant street life.",
+        imageUrl: "./images/bangkok.jpg"
+    },
+    {
+        title: "Vientiane", 
+        location: "Laos",
+        googleMapsUrl: "https://goo.gl/maps/Fw2xpNj3pVMhWBdf7",
+        startDate: "Feb 2015",
+        endDate: "Feb 2015",
+        description: "Vientiane, Laos' national capital, mixes French-colonial architecture with Buddhist temples. Vientiane is noted as the home of the most significant national monuments in Laos – That Luang.",
+        imageUrl: "./images/vientiane.webp"
+    },
+    {
+        title: "Koh Samui",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/ixW6Hy2c2eRJaZS57",
+        startDate: "Nov 2013",
+        endDate: "Nov 2013",
+        description: "Ko Samui, Thailand’s second largest island, lies in the Gulf of Thailand off the east coast of the Kra Isthmus. It's known for its palm-fringed beaches, coconut groves and dense, mountainous rainforest, plus luxury resorts and posh spas.",
+        imageUrl: "./images/koh-samui.jpg"
+    },
+    {
+        title: "Phuket",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/3vEFMvRQZNpmCj4g6",
+        startDate: "July 2011", 
+        endDate: "July 2011",
+        description: "Phuket is the largest island in Thailand. It has some of Thailand’s most popular beaches, mainly situated along the clear waters of the western shore.",
+        imageUrl: "./images/phuket.jpg"
+    },
+    {
+        title: "Koh Phi Phi Don",
+        location: "Thailand",
+        googleMapsUrl: "https://goo.gl/maps/AFTRiwkbZCX8aGkS6",
+        startDate: "July 2011", 
+        endDate: "July 2011", 
+        description: "Ko Phi Phi Don is the main island of Thailand’s Ko Phi Phi archipelago. It’s known for its white-sand beaches, including Long Beach and Loh Dalum Bay. Offshore, tiny Ko Phi Phi Leh is home to secluded Maya Bay, encircled by limestone cliffs.",
+        imageUrl: "./images/koh-phi-phi.jpg"
+    }
+]
