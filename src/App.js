3d8808314b0abcfb8f8f220ad84b7d81e3a891d6
Jethro Dean <jethroldean@gmail.com>
2022-03-31 15:48:39 +1100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Cards from "./components/Cards"
+import Navbar from "./components/Navbar"
+import data from "./data"
+
+export default function App() {
+    return (
+    <div>
+        <Navbar />
+        <Cards />
+    </div>
+    )
+}
