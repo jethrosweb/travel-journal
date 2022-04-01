8433b1e11fd329558e4763a4203e54b59b794c3a
Jethro Dean <jethroldean@gmail.com>
2022-04-01 13:57:09 +1100

    Styling complete - project complete

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,17 +4,19 @@ export default function Cards(props) {
     return (
         <div className="cards">
             <img src={props.item.imageUrl} alt="Location photo" className="cards--images" />
-            <section>
-                <ion-icon name="pin"></ion-icon>
-                <p>{props.item.location}</p>
-                <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-            </section>
-            <h1>{props.item.title}</h1>
-            <p>
-                <span>{props.item.startDate}</span>-
-                <span>{props.item.endDate}</span>
-            </p>
-            <p>{props.item.description}</p>
+            <div>
+                <section className="cards--location">
+                    <ion-icon name="pin"></ion-icon>
+                    <p>{props.item.location.toUpperCase()}</p>
+                    <a href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a>
+                </section>
+                <h1 className="cards--title">{props.item.title}</h1>
+                <p className="cards--dates">
+                    <span>{props.item.startDate} </span>-
+                    <span> {props.item.endDate}</span>
+                </p>
+                <p className="cards--description">{props.item.description}</p>
+            </div>
         </div>
     )
 }
